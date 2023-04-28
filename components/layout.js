901732ed842642bfe69ffe1be4eea07e9ec055f3
anderskambenders <a.zetsmanis@mail.ru>
2023-04-28 15:02:19 +0300

    feat: add keyboard layout

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,8 @@
+// eslint-disable-next-line import/extensions
+import keysObj from './keyboard.js';
+
 function initLayout() {
+  const keyWide = ['Backspace', 'CapsLock', 'Enter', 'Tab', 'ControlLeft'];
   const wrapper = document.createElement('section');
   const title = document.createElement('h1');
   const textarea = document.createElement('textarea');
@@ -16,5 +20,18 @@ function initLayout() {
   desc.classList.add('desc');
   desc.innerHTML = 'Keyboard is created on MacOS. Use "shift" + "command" to switch language';
   wrapper.append(desc);
+  Object.entries(keysObj).forEach((key) => {
+    const keyElement = document.createElement('div');
+    const [keyEvent, keyVal] = key;
+    keyElement.classList.add('key');
+    if (keyWide.includes(keyEvent)) {
+      keyElement.classList.add('key_wide');
+    }
+    if (keyEvent === 'Space') {
+      keyElement.classList.add('key_space');
+    }
+    keyElement.innerHTML = keyVal;
+    keyboard.append(keyElement);
+  });
 }
 export default initLayout;
