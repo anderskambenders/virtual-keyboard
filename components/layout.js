a233a06d250202e307161739684a8dd7c270d8b1
anderskambenders <a.zetsmanis@mail.ru>
2023-04-30 01:06:47 +0300

    feat: add ru language

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,7 @@ function initLayout() {
   const textarea = document.createElement('textarea');
   const keyboard = document.createElement('div');
   const desc = document.createElement('p');
+  let lang = 'En';
   wrapper.classList.add('wrapper');
   document.body.append(wrapper);
   title.classList.add('title');
@@ -20,24 +21,29 @@ function initLayout() {
   desc.classList.add('desc');
   desc.innerHTML = 'Keyboard is created on MacOS. Use "shift" + "command" to switch language';
   wrapper.append(desc);
-  Object.entries(keysObj).forEach((key) => {
-    const keyElement = document.createElement('div');
-    const [keyEvent, keyVal] = key;
-    keyElement.classList.add(`${keyEvent}`, 'key');
-    if (keyWide.includes(keyEvent)) {
-      keyElement.classList.add('key_wide');
-    }
-    if (keyEvent === 'Space') {
-      keyElement.classList.add('key_space');
-    }
-    keyElement.innerHTML = keyVal;
-    keyboard.append(keyElement);
-  });
+  const initKeys = () => {
+    Object.entries(keysObj).forEach((key) => {
+      const keyElement = document.createElement('div');
+      const [keyEvent, keyVal] = key;
+      keyElement.classList.add(`${keyEvent}`, 'key');
+      if (keyWide.includes(keyEvent)) {
+        keyElement.classList.add('key_wide');
+      }
+      if (keyEvent === 'Space') {
+        keyElement.classList.add('key_space');
+      }
+      const [keyValEn, keyValRu] = keyVal;
+      keyElement.innerHTML = (lang === 'En' ? keyValEn : keyValRu);
+      keyboard.append(keyElement);
+    });
+  };
+
   const textareaKeyboardClick = (keyEvent) => {
     const startArea = textarea.selectionStart;
     const endArea = textarea.selectionEnd;
     const textareaText = textarea.value.substring(0, startArea)
-              + keysObj[keyEvent.classList[0]] + textarea.value.substring(endArea);
+              + keysObj[keyEvent.classList[0]][1] + textarea.value.substring(endArea);
+    textarea.focus();
     textarea.value = textareaText;
     textarea.focus();
     textarea.selectionEnd = (startArea === endArea) ? (endArea + 1) : endArea;
@@ -61,13 +67,14 @@ function initLayout() {
   };
   const keyboardClick = () => {
     document.addEventListener('keydown', (keyEvent) => {
+      textarea.focus();
       document.querySelector(`.${keyEvent.code}`).classList.add('key_active');
     });
     document.addEventListener('keyup', (keyEvent) => {
       document.querySelector(`.${keyEvent.code}`).classList.remove('key_active');
     });
   };
-
+  initKeys();
   mouseClick();
   keyboardClick();
 }
